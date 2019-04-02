c3336881a62b8201b9015935f212414cbfd673ec
anna <annapiwonska1@gmail.com>
2019-04-02 16:31:37 +0200

    Add pagination and search to blog app

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='posts'),
+    path('results', views.search, name='search'),
     path('post/<int:post_id>', views.post_detail, name='post'),
     path('category/<int:category_id>', views.category_detail, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Post
+from django.db.models import Q
+from django.core.paginator import Paginator
 
-# Create your views here.
 
 def post_list(request):
-	posts = Post.objects.all()
-	categories = Category.objects.all()
+	post_list = Post.objects.all()
+	paginator = Paginator(post_list, 3)
+	page = request.GET.get('page')
+	posts = paginator.get_page(page)
+
+	categories = Category.objects.all()	
+
 	return render(request, "blog/post_list.html", {'posts':posts, 'categories': categories})
 
 def post_detail(request, post_id):
@@ -16,3 +22,17 @@ def category_detail(request, category_id):
 	category = get_object_or_404(Category, id=category_id)
 	categories = Category.objects.all()
 	return render(request, "blog/category_detail.html",{'category':category, 'categories': categories})
+
+def search(request):
+	query = request.GET.get('q')
+	if query:
+	 results = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query))
+	else:
+		 results = Post.objects.all()
+	paginator = Paginator(results, 3)
+	page = request.GET.get('page')
+	posts = paginator.get_page(page)
+
+	categories = Category.objects.all()	
+
+	return render(request, "blog/post_list.html", {'posts':posts, 'categories': categories, 'query':query})
